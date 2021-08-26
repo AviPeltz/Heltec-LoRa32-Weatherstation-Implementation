ea1d98e0492894c59e7164dd95a5e3ebdc476562
AviPeltz <aj.peltz@gmail.com>
2021-08-26 11:59:51 -0700

    Update OLED_LoRa_Receiver.ino

--- OLED_LoRa_Receiver/OLED_LoRa_Receiver.ino
+++ OLED_LoRa_Receiver/OLED_LoRa_Receiver.ino
@@ -20,7 +20,7 @@
 #include "heltec.h" 
 #include "images.h"
 
-#define BAND    433E6  //you can set band here directly,e.g. 868E6,915E6
+#define BAND    915E6  //you can set band here directly,e.g. 868E6,915E6
 String rssi = "RSSI --";
 String packSize = "--";
 String packet ;
